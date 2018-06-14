0d76daa8eecf2951bd2dc08d256afe583a2fe3b8
ezhov_da <ezhdenis@yandex.ru>
2018-06-14 22:46:09 +0300

    Добавил в фильтр проверку на открытые ресурсы

--- src/main/java/ru/ezhov/groovy/service/BasicAuthorisationFilter.java
+++ src/main/java/ru/ezhov/groovy/service/BasicAuthorisationFilter.java
@@ -32,29 +32,47 @@ public class BasicAuthorisationFilter implements Filter {
     }
 
     private void doBasicAuth(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        String authHeader = ((HttpServletRequest) servletRequest).getHeader("Authorization");
-        LOG.info("authHeader: " + authHeader);
-        if (authHeader == null) {
-            errorAuth(((HttpServletResponse) servletResponse));
+        if (publicSource((HttpServletRequest) servletRequest)) {
+            filterChain.doFilter(servletRequest, servletResponse);
         } else {
-            String afterClear = authHeader.replaceAll("Basic ", "");
-            LOG.info("afterClear: " + afterClear);
-            String lpass = new String(new BASE64Decoder().decodeBuffer(afterClear), "UTF-8");
-            LOG.info("lpass: " + lpass);
-            String[] arr = lpass.split(":");
-            if (arr.length < 2) {
+            String authHeader = ((HttpServletRequest) servletRequest).getHeader("Authorization");
+            LOG.info("authHeader: " + authHeader);
+            if (authHeader == null) {
                 errorAuth(((HttpServletResponse) servletResponse));
             } else {
-                String login = arr[0];
-                String pass = arr[1];
-                LOG.info("login: [" + login + "] pass:[" + pass + "]");
-                if (!"1".equals(login) && !"1".equals(pass)) {
+                String afterClear = authHeader.replaceAll("Basic ", "");
+                LOG.info("afterClear: " + afterClear);
+                String lpass = new String(new BASE64Decoder().decodeBuffer(afterClear), "UTF-8");
+                LOG.info("lpass: " + lpass);
+                String[] arr = lpass.split(":");
+                if (arr.length < 2) {
                     errorAuth(((HttpServletResponse) servletResponse));
                 } else {
-                    filterChain.doFilter(servletRequest, servletResponse);
+                    String login = arr[0];
+                    String pass = arr[1];
+                    LOG.info("login: [" + login + "] pass:[" + pass + "]");
+                    if (!"1".equals(login) && !"1".equals(pass)) {
+                        errorAuth(((HttpServletResponse) servletResponse));
+                    } else {
+                        filterChain.doFilter(servletRequest, servletResponse);
+                    }
                 }
             }
         }
+    }
+
+    private boolean publicSource(HttpServletRequest httpServletRequest) {
+        String requestInfo = httpServletRequest.getRequestURI();
+        LOG.info("request uri: " + requestInfo);
+        String[] array = requestInfo.split("/");
+        boolean isPublic = false;
+        for (String part : array) {
+            if (part != null && part.startsWith("pub")) {
+                isPublic = true;
+                break;
+            }
+        }
+        return isPublic;
     }
 
     private void errorAuth(HttpServletResponse servletResponse) throws IOException {
@@ -62,4 +80,5 @@ public class BasicAuthorisationFilter implements Filter {
         servletResponse.setStatus(401);
         servletResponse.getOutputStream().print("Sorry, but bad, very bad authorization");
     }
+
 }
